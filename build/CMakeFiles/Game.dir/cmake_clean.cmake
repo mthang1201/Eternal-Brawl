709d4ca47af2b34e23e5786325069d7ba73cc6e5
mthang1201 <89679587+mthang1201@users.noreply.github.com>
2024-02-29 09:56:24 +0700

    accidentally delete repo, now fixing it

---
+++ build/CMakeFiles/Game.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "C:/Users/Minh Thang Bui/source/repos/Game/bin/Game.exe"
+  "C:/Users/Minh Thang Bui/source/repos/Game/bin/Game.exe.manifest"
+  "C:/Users/Minh Thang Bui/source/repos/Game/bin/Game.pdb"
+  "C:/Users/Minh Thang Bui/source/repos/Game/bin/libGame.dll.a"
+  "CMakeFiles/Game.dir/src/entity.cpp.obj"
+  "CMakeFiles/Game.dir/src/entity.cpp.obj.d"
+  "CMakeFiles/Game.dir/src/main.cpp.obj"
+  "CMakeFiles/Game.dir/src/main.cpp.obj.d"
+  "CMakeFiles/Game.dir/src/math.cpp.obj"
+  "CMakeFiles/Game.dir/src/math.cpp.obj.d"
+  "CMakeFiles/Game.dir/src/renderWindow.cpp.obj"
+  "CMakeFiles/Game.dir/src/renderWindow.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- run.ps1
+++ run.ps1
@@ -1,14 +1,12 @@
-$exeFiles = Get-ChildItem -Path "bin" -Filter *.exe
-
 if ($exeFiles.Count -gt 0) {
     rm bin/Game.exe
 }
 
-& cd build
-& cmake -G "MinGW Makefiles" ..
-# & cmake -G "CodeBlocks - MinGW Makefiles" ..
-& cmake --build .
+cd build
+cmake -G "MinGW Makefiles" ..
+# cmake -G "CodeBlocks - MinGW Makefiles" ..
+cmake --build .
 
 if ($LASTEXITCODE -eq 0) {
-    & ./../bin/Game
+    ./../bin/Game
 }
