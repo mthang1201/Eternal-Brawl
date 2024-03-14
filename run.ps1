9258503a80fe28d46722c78015bace2fc3b23284
mthang1201 <89679587+mthang1201@users.noreply.github.com>
2024-03-14 08:41:15 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -2,11 +2,20 @@ if ($exeFiles.Count -gt 0) {
     rm bin/Game.exe
 }
 
+# FOR CMAKE
 cd build
 cmake -G "MinGW Makefiles" ..
-# cmake -G "CodeBlocks - MinGW Makefiles" ..
 cmake --build .
 
 if ($LASTEXITCODE -eq 0) {
     ./../bin/Game
 }
+
+
+# FOR MAKEFILE
+# make
+
+# if ($LASTEXITCODE -eq 0) {
+#     cd bin
+#     ./Game
+# }
